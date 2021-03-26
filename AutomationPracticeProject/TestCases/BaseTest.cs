da50a330fb2b2e9abae11b410640dfacca50d9fb
Aliaksandra Rybakova <Aliaksandra.Rybakova@itechart-group.com>
2021-03-26 11:42:49 +0300

    add AutomationPracticeProject

---
+++ AutomationPracticeProject/TestCases/BaseTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Configuration;
+using AutomationPracticeProject.Helpers;
+using AutomationPracticeProject.WrapperFactory;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace AutomationPracticeProject.TestCases
+{
+    [AllureNUnit]
+    [TestFixture]
+    public class BaseTest 
+    {
+        [SetUp]
+        public void SetUpTest()
+        {
+            ExtentReportsHelper.GetExtentReportsHelper().CreateTest(TestContext.CurrentContext.Test.Name);
+            WebDriverFactory.InitBrowser("Chrome");
+            LogHelper.Info("Browser started.");
+            WebDriverFactory.GoToUrl(ConfigurationManager.AppSettings["URL"]);
+            LogHelper.Info($"Browser navigated to the url [{ConfigurationManager.AppSettings["URL"]}].");
+            WebDriverFactory.Driver.Manage().Window.Maximize();
+            LogHelper.Info("Browser maximized.");
+            WebDriverFactory.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            try
+            {
+                var status = TestContext.CurrentContext.Result.Outcome.Status;
+                var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+                var errorMessage = "<pre>" + TestContext.CurrentContext.Result.Message + "</pre>";
+                
+                switch (status)
+                {
+                    case TestStatus.Failed:
+                        ExtentReportsHelper.GetExtentReportsHelper().SetTestStatusFail($"<br>{errorMessage}<br>Stack Trace: <br>{stacktrace}<br>");
+                        break;
+                    case TestStatus.Skipped:
+                        ExtentReportsHelper.GetExtentReportsHelper().SetTestStatusSkipped();
+                        break;
+                    default:
+                        ExtentReportsHelper.GetExtentReportsHelper().SetTestStatusPass();
+                        break;
+                }
+            }
+            catch (Exception exc)
+            {
+                throw (exc);
+            }
+            finally
+            {
+                WebDriverFactory.CloseAllDrivers();
+                LogHelper.Info("Browser closed.");
+            }
+        }
+    }
+}
