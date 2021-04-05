0a9eac654620cdd5518452e413a1665b2d1eee8f
Aliaksandra Rybakova <Aliaksandra.Rybakova@itechart-group.com>
2021-04-05 17:49:20 +0300

    add RegistrationWithValidCredentials Api test

--- AutomationPracticeProject/ApiTestCases/Authentication/Login.cs
+++ AutomationPracticeProject/ApiTestCases/Authentication/Login.cs
@@ -1,30 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using AutomationPracticeProject.Constants;
+using AutomationPracticeProject.Helpers;
 using NUnit.Framework;
 
 namespace AutomationPracticeProject.ApiTestCases.Authentication
 {
     public class Login
     {
+        private HttpClient _client = new HttpClient();
+        private string _contentType = "application/x-www-form-urlencoded";
+
         [Test, Category("Priority_High")]
         public void LoginWithValidCredentials()
         {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Post, "http://automationpractice.com/index.php");
-            var postData = new FormUrlEncodedContent(
+            var loginData = new FormUrlEncodedContent(
                 new List<KeyValuePair<string, string>>
                 {
                     new KeyValuePair<string, string>("controller", "authentication"),
-                    new KeyValuePair<string, string>("email", "lizy.flower22@gmail.com"),
-                    new KeyValuePair<string, string>("passwd", "12345"),
+                    new KeyValuePair<string, string>("email", ConfigurationManager.AppSettings["Login"]),
+                    new KeyValuePair<string, string>("passwd", ConfigurationManager.AppSettings["Password"]),
                     new KeyValuePair<string, string>("SubmitLogin", "")
                 }
             );
-            request.Content = postData;
-            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
-            var response = client.SendAsync(request);
+            
+            var response = ApiHelper.SendPostRequest(_client, EndPoints.BasePath, loginData, _contentType);
+            Console.WriteLine(response.Result.Content.ReadAsStringAsync().Result);
+        }
+
+        [Test, Category("Priority_High")]
+        public void RegistrationWithValidCredentials()
+        {
+            var registrationEmail = $"{RandomHelper.GetRandomStringWithNumbers(8)}@mail.ru";
+            var firstName = $"FirstName{RandomHelper.GetRandomString(8)}";
+            var lastName = $"LastName{RandomHelper.GetRandomString(8)}";
+            var password = $"Password{RandomHelper.GetRandomStringWithNumbers(8)}";
+            var address = $"Address{RandomHelper.GetRandomString(8)}";
+            var city = $"City{RandomHelper.GetRandomString(8)}";
+            var zipCode = $"{RandomHelper.GetRandomNumbers(5)}";
+            var mobilePhone = $"{RandomHelper.GetRandomNumbers(8)}";
+            var addressAlias = $"AddressAlias{RandomHelper.GetRandomString(8)}";
+
+            var registrationData = new FormUrlEncodedContent(
+                new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("controller", "authentication"),
+                    new KeyValuePair<string, string>("customer_firstname", firstName),
+                    new KeyValuePair<string, string>("customer_lastname", lastName),
+                    new KeyValuePair<string, string>("email", registrationEmail),
+                    new KeyValuePair<string, string>("passwd", password),
+                    new KeyValuePair<string, string>("firstname", firstName),
+                    new KeyValuePair<string, string>("lastname", lastName),
+                    new KeyValuePair<string, string>("address1", address),
+                    new KeyValuePair<string, string>("city", city),
+                    new KeyValuePair<string, string>("id_state", StatesDropdownConstants.Arkansas),
+                    new KeyValuePair<string, string>("postcode", zipCode),
+                    new KeyValuePair<string, string>("id_country", CountriesDropdownConstants.UnitedStates),
+                    new KeyValuePair<string, string>("phone_mobile", mobilePhone),
+                    new KeyValuePair<string, string>("alias", addressAlias),
+                    new KeyValuePair<string, string>("submitAccount", "")
+                }
+            );
+
+            var response = ApiHelper.SendPostRequest(_client, EndPoints.BasePath, registrationData, _contentType);
             Console.WriteLine(response.Result.Content.ReadAsStringAsync().Result);
         }
     }
--- AutomationPracticeProject/Helpers/ApiHelper.cs
+++ AutomationPracticeProject/Helpers/ApiHelper.cs
@@ -1,7 +1,17 @@
-﻿namespace AutomationPracticeProject.Helpers
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AutomationPracticeProject.Helpers
 {
     public static class ApiHelper
     {
-        
+        public static Task<HttpResponseMessage> SendPostRequest(HttpClient client, string requestUri, HttpContent requestContent, string contentType)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, requestUri) {Content = requestContent};
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+            var response = client.SendAsync(request);
+            return response;
+        }
     }
 }
