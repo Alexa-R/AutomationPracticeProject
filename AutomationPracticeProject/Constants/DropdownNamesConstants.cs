9d3a52b09bf92486800a61106c1c1e8b40f8bc11
Aliaksandra Rybakova <Aliaksandra.Rybakova@itechart-group.com>
2021-04-12 12:21:02 +0300

    add SortProductsOnCategoryPlp Test

--- AutomationPracticeProject/Constants/DropdownNamesConstants.cs
+++ AutomationPracticeProject/Constants/DropdownNamesConstants.cs
@@ -7,5 +7,6 @@
         public const string SubjectHeadingDropdown = "contact";
         public const string DeliveryAddressDropdown = "address_delivery";
         public const string BillingAddressDropdown = "address_invoice";
+        public const string SortDropdown = "selectProductSort";
     }
 }
---
+++ AutomationPracticeProject/TestCases/PLP/SortingRule.cs
@@ -0,0 +1,17 @@
+﻿using AutomationPracticeProject.Constants;
+using AutomationPracticeProject.PageObjects;
+using NUnit.Framework;
+
+namespace AutomationPracticeProject.TestCases.PLP
+{
+    public class SortingRule : BaseTest
+    {
+        [Test, Category("Priority_Medium")]
+        public void SortProductsOnCategoryPlp()
+        {
+            Pages.BasePage.ClickProductCategoryButton(ProductCategoryNamesConstants.Women);
+            Pages.BasePage.ClickDropdown(DropdownNamesConstants.SortDropdown);
+            Pages.BasePage.ClickOptionFromDropdown(DropdownNamesConstants.SortDropdown, SortDropdownConstants.PriceHighestFirst);
+        }
+    }
+}
